af1f25a70e8869faefa42fc499c83c8613efcc80
YeahO_O Yuan <yzwduck@gmail.com>
2019-01-01 11:54:39 +0800

    ci: test cache

--- .ci/install.ps1
+++ .ci/install.ps1
@@ -1,1 +1,31 @@
+# expected working directory: .\.ci
+
+# accepted arguments:
+param (
+	[string]$tldir = 'C:\texlive'
+)
+
+# install Chinese Simplified fonts
+Set-Service BITS -StartupType Automatic
+Set-Service wuauserv -StartupType Automatic
+Start-Service BITS
+Start-Service wuauserv
+Import-Module Dism
+Get-WindowsCapability -Online | where { $_.Name -match "Font" -and $_.Name -match "Hans" } | Add-WindowsCapability -Online
+
+# if not cached, install texlive
+if ((Get-Command "tlmgr" -ErrorAction SilentlyContinue) -eq $null) {
+	curl.exe -fsSL -o install-tl.zip "http://mirror.ctan.org/systems/texlive/tlnet/install-tl.zip"
+	7z.exe x install-tl.zip -y
+	Remove-Item install-tl.zip
+	Remove-Item -Path "$tldir" -recurse --Force
+	mkdir -Force "$tldir"
+	copy .\texlive-windows.profile "$tldir\texlive.profile"
+	dir | where { $_ -match "$install-tl" -and $_ -notmatch "install-tl.zip" } | Rename-Item -NewName "install-tl"
+	.\install-tl\install-tl-windows.bat -profile "$tldir\texlive.profile"
+}
+
+# update texlive
 tlmgr install (Get-Content .\texlive.pkgs | where { $_ -notmatch "(^\s*#)|(^\s*$)" })
+tlmgr update --all
+tlmgr update --all
