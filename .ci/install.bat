f7c80f66fdca3fa51d36b95b67b9f835c4aba260
bismarckkk <zuoqingyu@nuaa.edu.cn>
2026-01-11 14:59:47 +0800

    change ci to github action

--- .ci/install.bat
+++
@@ -1,1 +0,0 @@
-powershell -ExecutionPolicy RemoteSigned -File .\install.ps1
--- .github/pack.sh
+++ .github/pack.sh
@@ -6,13 +6,13 @@
 # To make user easy to start up, the pack should be as small as possible.
 
 TMP_DIR_REL=.pack/nuaathesis
-rm -rf $TMP_DIR
+rm -rf $TMP_DIR_REL
 mkdir -p $TMP_DIR_REL
 TMP_DIR=$(realpath $TMP_DIR_REL)
 
 cp -r .git $TMP_DIR
 (cd $TMP_DIR && git reset --hard && git clean -Xf && rm -rf .* || true)
-cp -r .ci $TMP_DIR
+# cp -r .ci $TMP_DIR  # .ci directory not present in workspace
 
 find . -not -path '*/\.*' -type f \( -name '*.pdf' -o -name '*.cls' -o -name '*.bst' \) -exec cp --parents '{}' $TMP_DIR \;
 # curl -o $TMP_DIR/.gitignore https://raw.githubusercontent.com/github/gitignore/master/TeX.gitignore
