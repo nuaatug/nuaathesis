67bafa6a893f5b05cec9433d7d29f5d9005d9098
bismarckkk <zuoqingyu@nuaa.edu.cn>
2026-03-15 14:42:04 +0800

    make build.bat also has -all param to debug

--- build.bat
+++ build.bat
@@ -4,3 +4,9 @@ makeindex -s gglo.ist -o nuaathesis.gls nuaathesis.glo &&^
 xelatex nuaathesis.dtx &&^
 xelatex nuaathesis.dtx &&^
 xelatex nuaathesis.dtx
+
+if "%1"=="-all" (
+  make -C demo_chs &&^
+  make -C demo_en &&^
+  make -C demo_ja
+)
